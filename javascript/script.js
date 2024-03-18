985416e81175dc4a5aae701f7fc31dd324fe72de
Kishou <emmanuellugo84@gmail.com>
2024-03-17 22:58:13 -0400

    buttons added for each color

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,50 @@
-let backGroundColor ="";
-let selection="";
-let submitButton="";
+const redButton = document.getElementById("red");
+const blueButton = document.getElementById("blue");
+const yellowButton = document.getElementById("yellow");
+const greenButton = document.getElementById("green");
+const purpleButton = document.getElementById("purple");
+const orangeButton = document.getElementById("orange");
 
 
+
+const changBackGroundToRed = () => {
+    document.getElementById("main").style.backgroundColor = "red";
+}
+
+const changBackGroundToBlue = () => {
+    document.getElementById("main").style.backgroundColor = "blue";
+}
+
+const changBackGroundToYellow = () => {
+    document.getElementById("main").style.backgroundColor = "yellow";
+}
+
+const changBackGroundToGreen = () => {
+    document.getElementById("main").style.backgroundColor = "green";
+}
+
+const changBackGroundToPurple = () => {
+    document.getElementById("main").style.backgroundColor = "purple";
+}
+
+const changBackGroundToOrange = () => {
+    document.getElementById("main").style.backgroundColor = "orange";
+}
+
+
+redButton.addEventListener("click",changBackGroundToRed)
+
+
+blueButton.addEventListener("click",changBackGroundToBlue)
+
+
+yellowButton.addEventListener("click",changBackGroundToYellow)
+
+
+greenButton.addEventListener("click",changBackGroundToGreen)
+
+
+purpleButton.addEventListener("click",changBackGroundToPurple)
+
+
+orangeButton.addEventListener("click",changBackGroundToOrange)
